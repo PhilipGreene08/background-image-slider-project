2dedef18cc05bf902de1e70537a20415ee5f8a81
PhilipGreene08 <54162361+PhilipGreene08@users.noreply.github.com>
2021-04-10 22:06:01 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,41 @@
-// immediate invoked function expression
+const rightClicker = document.querySelector('.btn-right')
+const leftClicker = document.querySelector('.btn-left')
+const image = document.querySelector('.img-container')
 
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+let picCounter = 0
 
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
+//data (pics)
+const pictures = [
+  'contBcg-0',
+  'contBcg-1',
+  'contBcg-2',
+  'contBcg-3',
+  'contBcg-4',
+]
 
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-    })
-  })
-})();
+function nextPic(e) {
+  picCounter++
+  if (picCounter > pictures.length - 1) {
+    picCounter = 0
+    image.style.backgroundImage = `url('./img/${pictures[picCounter]}.jpeg')`
+  } else {
+    image.style.backgroundImage = `url('./img/${pictures[picCounter]}.jpeg')`
+    console.log(picCounter);
+  }
+  e.preventDefault()
+}
+
+function previousPic(e) {
+  picCounter--
+  if (picCounter < 0) {
+    picCounter = pictures.length - 1
+    image.style.backgroundImage = `url('./img/${pictures[picCounter]}.jpeg')`
+  } else {
+    image.style.backgroundImage = `url('./img/${pictures[picCounter]}.jpeg')`
+    console.log(picCounter);
+  }
+  e.preventDefault()
+}
+
+rightClicker.addEventListener('click', nextPic)
+leftClicker.addEventListener('click', previousPic)
